23b570ef625dfe96b273e3bb5de3f6c051fdd0aa
KIMCHAN\KCLAB <ckim.esw@gmail.com>
2022-09-18 07:35:59 +0900

    Update: numpy

--- 2022-09-20/documents/numpy_example.py
+++ 2022-09-20/documents/numpy_example.py
@@ -1,6 +1,6 @@
-# example1
 import numpy as np
 
+# example1
 zeros = np.zeros((2, 5), np.int32)
 ones = np.ones((3, 1), np.uint8)
 emptys = np.empty((1, 5), np.float64)
@@ -10,3 +10,12 @@ print(zeros)
 print(ones)
 print(emptys)
 print(fulls)
+
+# example2
+np.random.seed(10)
+a = np.random.rand(2, 3)
+b = np.random.randint(1, 100, 6)
+print(a)
+print(b)
+b = b.reshape(2,-1)
+print(b)
