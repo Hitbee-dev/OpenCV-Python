a6b50f670da612a8afd6d0146ee41165f4a215bd
KIMCHAN\KCLAB <ckim.esw@gmail.com>
2022-09-17 16:27:53 +0900

    Update: function

---
+++ 2022-09-20/documents/function_example.py
@@ -0,0 +1,22 @@
+# 코드의 재사용 (구구단)
+## solution1 (for)
+for i in range(1, 10):
+    print(f"{2} x {i} = {2*i}")
+for i in range(1, 10):
+    print(f"{3} x {i} = {2*i}")
+# ...
+for i in range(1, 10):
+    print(f"{8} x {i} = {2*i}")
+for i in range(1, 10):
+    print(f"{9} x {i} = {2*i}")
+
+## solution2 (function)
+def gogodan(num):
+    for i in range(1, 10):
+        print(f"{num} x {i} = {num*i}")
+
+gogodan(2)
+gogodan(3)
+# ...
+gogodan(8)
+gogodan(9)
