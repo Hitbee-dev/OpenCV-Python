86045f1eb77dcbb01afce40b1eca61470fa214ab
KIMCHAN\KCLAB <ckim.esw@gmail.com>
2022-09-18 00:01:15 +0900

    임시 커밋

---
+++ 2022-09-20/documents/numpy_example.py
@@ -0,0 +1,12 @@
+# example1
+import numpy as np
+
+zeros = np.zeros((2, 5), np.int32)
+ones = np.ones((3, 1), np.uint8)
+emptys = np.empty((1, 5), np.float64)
+fulls = np.full(5, 15, np.float32)
+
+print(zeros)
+print(ones)
+print(emptys)
+print(fulls)
