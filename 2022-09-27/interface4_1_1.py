b1a435136f0a4ca9a5bbf737b96dc046d6412e8f
Hitbee-dev <ckim.esw@gmail.com>
2022-09-26 06:15:38 +0900

    Update: 4_1_2

--- 2022-09-27/interface4_1_1.py
+++ 2022-09-27/interface4_1_1.py
@@ -2,17 +2,10 @@ import numpy as np
 import cv2
 
 image = np.zeros((200, 400), np.uint8)
-image[:] = 200
-
 print(image)
 
-title1, title2 = 'Position1', 'Position2'
-cv2.namedWindow(title1, cv2.WINDOW_AUTOSIZE)
-cv2.namedWindow(title2)
-cv2.moveWindow(title1, 150, 150)
-cv2.moveWindow(title2, 400, 50)
-
+title1 = 'Position1'
+cv2.namedWindow(title1)
 cv2.imshow(title1, image)
-cv2.imshow(title2, image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- 2022-09-27/interface4_1_2.py
+++ 2022-09-27/interface4_1_2.py
@@ -2,10 +2,12 @@ import numpy as np
 import cv2
 
 image = np.zeros((200, 400), np.uint8)
+image[:] = 200
 print(image)
 
-title1 = 'Position1'
-cv2.namedWindow(title1)
+title1 = 'Changed'
+cv2.namedWindow(title1, cv2.WINDOW_NORMAL)
+cv2.resizeWindow(title1, 250, 100)
 cv2.imshow(title1, image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
