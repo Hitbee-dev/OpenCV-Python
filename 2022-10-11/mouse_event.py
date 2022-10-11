cb0d2eb206c662a439e29a24b1091b4c85265b54
Chan Kim <ckim.esw@gmail.com>
2022-10-11 12:19:01 +0900

    Add files via upload

---
+++ 2022-10-11/mouse_event.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2
+
+def onMouse(event, x, y, flags, param):
+    global title, pt
+    if event == cv2.EVENT_LBUTTONDOWN or event == cv2.EVENT_RBUTTONDOWN:
+        pt = (x, y)
+    elif event == cv2.EVENT_LBUTTONUP:
+        cv2.rectangle(image, pt, (x, y), (192, 0, 0), 2)
+        cv2.imshow(title, image)
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if flags & cv2.EVENT_FLAG_LBUTTON:
+            temp_image = image.copy()
+            pt2 = (x, y)
+            cv2.rectangle(temp_image, pt, pt2, (192, 192, 192), 2)
+            cv2.imshow(title, temp_image)
+        elif flags & cv2.EVENT_FLAG_RBUTTON:
+            temp_image = image.copy()
+            dx, dy = pt[0] - x, pt[1] - y
+            radius = int(np.sqrt(dx*dx + dy*dy))
+            cv2.circle(temp_image, pt, radius, (192, 192, 192), 2)
+            cv2.imshow(title, temp_image)
+    elif event == cv2.EVENT_RBUTTONUP:
+        dx, dy = pt[0] - x, pt[1] - y
+        radius = int(np.sqrt(dx*dx + dy*dy))
+        cv2.circle(image, pt, radius, (0, 0, 192), 2)
+        cv2.imshow(title, image)
+
+image = np.full((300, 300, 3), (255, 255, 255), np.uint8)
+pt = [-1, -1]
+title = 'Draw Event'
+
+cv2.imshow(title, image)
+cv2.setMouseCallback(title, onMouse)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
