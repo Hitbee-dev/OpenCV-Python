5b1c16ac2cc5b3178665fca2bac500841ef6bf04
Hitbee-dev <ckim.esw@gmail.com>
2022-11-08 20:14:12 +0900

    Update: 2022-11-08

--- 2022-11-08/re_histogram_example12.py
+++ 2022-11-08/re_histogram_example12.py
@@ -1,3 +1,4 @@
+'''
 import numpy as np, cv2
 import matplotlib.pyplot as plt
 
@@ -26,5 +27,34 @@ plt.bar(np.arange(64), hist_v.flatten(), color='k')
 
 plt.subplot(2, 2, 3)
 plt.bar(np.arange(64), hist_h.flatten(), color='k')
+
+plt.show()
+'''
+
+import numpy as np, cv2
+import matplotlib.pyplot as plt
+
+image = cv2.imread("D:/github/OpenCV-Python/2022-11-08/images/hannam.jpg", cv2.IMREAD_COLOR)
+if image is None: raise Exception("영상 파일 읽기 오류")
+
+project1 = cv2.reduce(image, 0, cv2.REDUCE_AVG).ravel().astype(int)
+project2 = cv2.reduce(image, 1, cv2.REDUCE_AVG).ravel().astype(int)
+
+#print(image.shape)
+#print(project1.shape, project2.shape)
+
+_, ax = plt.subplots(2,2)
+# image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+ax[0,0].imshow(image, aspect="auto", cmap="gray")
+ax[0,0].set_title("image")
+ax[0,0].get_xaxis().set_ticks([])
+ax[0,0].get_yaxis().set_ticks([])
+
+ax[0,1].barh(np.arange(project1.shape[0]), project1)
+ax[0,1].set_title("horizontal projection")
+
+ax[1,0].bar(np.arange(project2.shape[0]), project2)
+ax[1,0].invert_yaxis()
+ax[1,0].set_title("vertical projection")
 
 plt.show()
