7f7891b97d2199ecc822e8f4c60e70b322ac93fd
Hitbee-dev <ckim.esw@gmail.com>
2022-11-28 03:17:04 +0900

    Update: 2022-11-29

--- 2022-11-29/geometry_affine.py
+++ 2022-11-29/geometry_affine.py
@@ -1,5 +1,6 @@
 # 어핀 변환이라고도 하고, 어파인 변환이라고도 함.
 # 비례를 유지하는 기하학적 함수
+# 어파인 변환은 크기가 2x3이어야 함.
 
 '''
 OpenCV에서 제공하는 어파인 변환: cv2.warpAffine()
@@ -18,57 +19,46 @@ cv2.getRotationMatrix2D()
 '''
 import numpy as np, cv2
 import matplotlib.pyplot as plt
-'''
-image = cv2.imread("D:/github/OpenCV-Python/2022-11-29/images/opencv_icon.png", cv2.IMREAD_GRAYSCALE)
-
-size = image.shape[::-1]                            # 영상 크기(y, x) -> (x, y)
-center = np.divmod(size, 2)[0]                      # divmod = 몫과 나머지를 반환하는 함수,
-                                                    # 뒤에 있는 값으로 앞에 있는 값을 나눔
-center = (image.shape[1] // 2, image.shape[0] // 2) # 회전 변환 기준 좌표(영상 중심), 이렇게 구현해도 됨
-angle = 45                                          # 회전 각도(30도), 
-tr = (200, 0)                                       # 평행이동 값 지정
-scale = 0.65                                        # 크기 변경 비율 지정
-
-print(size, center)
-
-pt1 = np.array([(462, 173),(658, 509),(269, 514)], np.float32)
-pt2 = np.array([(642, 73),(638, 389),(229, 794)], np.float32)
-aff_mat = cv2.getAffineTransform(pt1, pt2)
-rot_mat = cv2.getRotationMatrix2D(center, angle, scale)
-
-dst1 = cv2.warpAffine(image, aff_mat, size)
-dst2 = cv2.warpAffine(image, rot_mat, size)
-cv2.imwrite("D:/github/OpenCV-Python/2022-11-29/images/opencv_img.png", dst2)
-
-cv2.imshow('image', image)
-cv2.imshow('affine', dst1)
-cv2.imshow('rotation', dst2)
-cv2.waitKey(0)
-'''
 
 image = cv2.imread("D:/github/OpenCV-Python/2022-11-29/images/opencv_icon.png", cv2.IMREAD_COLOR)
-cv2.imshow('image', image)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 h, w, c = image.shape
 
-'어파인 변환을 활용한 회전'
-center = (w // 2, h // 2)
-angle = 45
-scale = 1
-rot_M = cv2.getRotationMatrix2D(center, angle, scale)
-dst1 = cv2.warpAffine(image, rot_M, (w, h))
-cv2.imshow("dst1", dst1)
-
 '어파인 변환을 활용한 이동'
-M = np.array([[1, 0, -100],
-              [0, 1, 200]], dtype=np.float32)
+a, b = -100, 200
+mov_M = np.array([[1, 0, a],
+                  [0, 1, b]], dtype=np.float32)
 # x축으로 100, y축으로 200 이동
+image_move = cv2.warpAffine(image, mov_M, (w, h))
 
-dst2 = cv2.warpAffine(image, M, (w, h))
-cv2.imshow('dst2', dst2)
+'어파인 변환을 활용한 회전'
+# center = (w // 2, h // 2)
+# angle = 45
+# scale = 1
+# rot_M = cv2.getRotationMatrix2D(center, angle, scale)
+# dst2 = cv2.warpAffine(image, rot_M, (w, h))
+theta = np.radians(45)
+rot_M = np.array([[np.cos(theta), np.sin(theta), (1-np.cos(theta))*w//2 - np.sin(theta)*h//2],
+                  [-np.sin(theta), np.cos(theta), np.sin(theta)*w//2 + (1-np.cos(theta))*h//2]], dtype=np.float32)
+image_rotate = cv2.warpAffine(image, rot_M, (w, h))
 
 '어파인 변환을 이용한 크기 변환'
-
-
+# 회전을 하면서 scale값을 변경하여 크기를 한번에 바꿀 수도 있지만, 따로 변환도 가능하다.
+# 방법1
+# resize_M = cv2.getRotationMatrix2D(center, 0, 0.5)
+# dst3 = cv2.warpAffine(image, resize_M, (w, h))
+# 방법2
+s_x, s_y = 0.7, 0.7
+resize_M = np.array([[s_x, 0, 0],
+                     [0, s_y, 0]], dtype=np.float32)
+image_resize = cv2.warpAffine(image, resize_M, (w, h))
 
 'matplotlib을 이용해 한번에 출력'
-cv2.waitKey(0)
+titles = ['image', 'image_move', 'image_rotate', 'image_resize']
+plt.figure(figsize=(12, 10))
+for idx, title in enumerate(titles):
+    plt.subplot(2, 2, idx+1)
+    plt.axis("off")
+    plt.title(title)
+    plt.imshow(eval(title))
+plt.show()
